2246656f470b7cdffc4284bd575445b2c979845f
owlyme <chudequan_48@163.com>
2018-03-08 18:29:46 +0800

    4.3.2-29

--- express/meadowlark.js
+++ express/meadowlark.js
@@ -1,31 +1,45 @@
 import express from 'express'
+import handler from 'express3-handlebars'
 
 const l = console.log;
+
 const app = express();
 
-
+const handlebars = handler.create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
-	res.type('text/plain');
-	res.send('Meadowlark Travel');
+	res.render('home');
 });
+
+var fortunes = [
+"Conquer your fears or they will conquer you.",
+"Rivers need springs.",
+"Do not fear what you don't know.",
+"You will have a pleasant surprise.",
+"Whenever possible, keep it simple.",
+];
 app.get('/about', function(req, res){
-	res.type('text/plain');
-	res.send('About Meadowlark Travel');
+	var randomFortune =fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', { fortune: randomFortune });
 });
 // 定制404 页面
 app.use(function(req, res){
 	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 // 定制500 页面
 app.use(function(err, req, res, next){
 	console.error(err.stack);
 	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
--- express/readme.txt
+++ express/readme.txt
@@ -9,3 +9,13 @@ HTTP
 件。这样在解决重定向问题或者不正确的状态码时会更加容易，它们经常被
 忽视。对于Chrome 来说，Ayima 的Redirect Path 特别好用。在大多数浏览
 器中， 都能在开发者工具的网络部分看到状态码。
+
+
+路由和中间件的添加顺序至关重要
+
+因此我推荐使用另外一个抽象程度较低的模板框架Handlebars。
+
+GIT 
+如果你不了解Git，建议你看一下Jon Loeliger 的Version Control with Git (O’Reilly，http://
+shop.oreilly.com/product/9780596520137.do）。另外，Code School 也有很好的Git 入门课程
+（https://try.github.io/）。
