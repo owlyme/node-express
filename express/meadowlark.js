2e1922ec0735be788c234221f648ff65a0e86e21
owlyme <chudequan_48@163.com>
2018-03-08 17:25:28 +0800

    v-1.0.17

---
+++ express/meadowlark.js
@@ -0,0 +1,35 @@
+import express from 'express'
+
+const l = console.log;
+const app = express();
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+	res.type('text/plain');
+	res.send('Meadowlark Travel');
+});
+app.get('/about', function(req, res){
+	res.type('text/plain');
+	res.send('About Meadowlark Travel');
+});
+// 定制404 页面
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+// 定制500 页面
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+	console.log( 'Express started on http://localhost:' +
+	app.get('port') + '; press Ctrl-C to terminate.' );
+})
+
---
+++ express/readme.txt
@@ -0,0 +1,11 @@
+
+1.Boilerplate（http://html5boilerplate.com/）
+
+babel 
+	npm install --save-dev babel-cli babel-preset-env
+
+HTTP
+安装一个能显示HTTP 请求状态码和所有重定向的浏览器插
+件。这样在解决重定向问题或者不正确的状态码时会更加容易，它们经常被
+忽视。对于Chrome 来说，Ayima 的Redirect Path 特别好用。在大多数浏览
+器中， 都能在开发者工具的网络部分看到状态码。
